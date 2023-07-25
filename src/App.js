77d75b5d65ce18730610fcdee6d775ce92f6e4ed
vitaly171 <72026466+vitaly171@users.noreply.github.com>
2023-07-25 14:38:49 +0300

    Products

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import Products from "./components/Products/Products";
 
 export default function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Products />
     </>
   );
 }
