bb8b6c4fe8135ba9bc38f81ef965a4c0d1f59613
vitaly171 <72026466+vitaly171@users.noreply.github.com>
2023-08-22 13:08:54 +0300

    advantage final

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Products from "./components/Products/Products";
 import About from "./components/About/About";
+import Advantages from "./components/Advantages/Advantages";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Hero />
         <Products />
         <About />
+        <Advantages />
       </main>
     </>
   );
