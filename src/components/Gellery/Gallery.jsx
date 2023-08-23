2fca71727872c398bfd0d87f6ea747c86bff2384
vitaly171 <72026466+vitaly171@users.noreply.github.com>
2023-08-23 14:53:27 +0300

    gallery commit

--- src/components/Gellery/Gallery.jsx
+++ src/components/Gellery/Gallery.jsx
@@ -7,6 +7,70 @@ export default function Gallery() {
     <section className={s.gallery} id="gallery">
       <div className={s.container}>
         <h2 className={s.visually_hidden}>Gallery</h2>
+        <div className={s.gallery__grid}>
+          <div className={classNames(s.gallery__item, s.gallery__item__pt4)}>
+            <img
+              classNames={s.gallery__img}
+              src={require(`../../img/gallery/gallery1@2x.jpg`)}
+              alt="Ice-cream"
+            />
+          </div>
+          <div className={classNames(s.gallery__item, s.gallery__item__pt4)}>
+            <img
+              classNames={s.gallery__img}
+              src={require(`../../img/gallery//gallery2@2x.jpg`)}
+              alt="Ice-cream"
+            />
+            <img
+              classNames={s.gallery__img}
+              src={require(`../../img/gallery/gallery3@2x.jpg`)}
+              alt="Ice-cream"
+            />
+          </div>
+          <div className={classNames(s.gallery__item, s.gallery__item__pt4)}>
+            <img
+              classNames={s.gallery__img}
+              src={require(`../../img/gallery/gallery4@2x.jpg`)}
+              alt="Ice-cream"
+            />
+            <img
+              classNames={s.gallery__img}
+              src={require(`../../img/gallery/gallery5@2x.jpg`)}
+              alt="Ice-cream"
+            />
+          </div>
+          <div className={classNames(s.gallery__item, s.gallery__item__pt4)}>
+            <img
+              classNames={s.gallery__img}
+              src={require(`../../img/gallery/gallery6@2x.jpg`)}
+              alt="Ice-cream"
+            />
+            <img
+              classNames={s.gallery__img}
+              src={require(`../../img/gallery/gallery7@2x.jpg`)}
+              alt="Ice-cream"
+            />
+          </div>
+          <div className={classNames(s.gallery__item, s.gallery__item__pt4)}>
+            <img
+              classNames={s.gallery__img}
+              src={require(`../../img/gallery/gallery8@2x.jpg`)}
+              alt="Ice-cream"
+            />
+            <img
+              classNames={s.gallery__img}
+              src={require(`../../img/gallery/gallery9@2x.jpg`)}
+              alt="Ice-cream"
+            />
+          </div>
+          <div className={classNames(s.gallery__item, s.gallery__item__pt4)}>
+            <img
+              classNames={s.gallery__img}
+              src={require(`../../img/gallery/gallery10@2x.jpg`)}
+              alt="Ice-cream"
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
