cdb75ac51a8e9a636518f00432c326ec4d09d3af
vitaly171 <72026466+vitaly171@users.noreply.github.com>
2023-08-23 15:16:00 +0300

    gallery

--- src/components/Gellery/Gallery.jsx
+++ src/components/Gellery/Gallery.jsx
@@ -10,62 +10,62 @@ export default function Gallery() {
         <div className={s.gallery__grid}>
           <div className={classNames(s.gallery__item, s.gallery__item__pt4)}>
             <img
-              classNames={s.gallery__img}
+              className={s.gallery__img}
               src={require(`../../img/gallery/gallery1@2x.jpg`)}
               alt="Ice-cream"
             />
           </div>
-          <div className={classNames(s.gallery__item, s.gallery__item__pt4)}>
+          <div className={classNames(s.gallery__item, s.gallery__item__pt2)}>
             <img
-              classNames={s.gallery__img}
+              className={s.gallery__img}
               src={require(`../../img/gallery//gallery2@2x.jpg`)}
               alt="Ice-cream"
             />
             <img
-              classNames={s.gallery__img}
+              className={s.gallery__img}
               src={require(`../../img/gallery/gallery3@2x.jpg`)}
               alt="Ice-cream"
             />
           </div>
-          <div className={classNames(s.gallery__item, s.gallery__item__pt4)}>
+          <div className={s.gallery__item}>
             <img
-              classNames={s.gallery__img}
+              className={classNames(s.gallery__img, s.gallery__img__2x)}
               src={require(`../../img/gallery/gallery4@2x.jpg`)}
               alt="Ice-cream"
             />
             <img
-              classNames={s.gallery__img}
+              className={classNames(s.gallery__img, s.gallery__img__1x)}
               src={require(`../../img/gallery/gallery5@2x.jpg`)}
               alt="Ice-cream"
             />
           </div>
-          <div className={classNames(s.gallery__item, s.gallery__item__pt4)}>
+          <div className={classNames(s.gallery__item, s.gallery__item__pt3)}>
             <img
-              classNames={s.gallery__img}
+              className={s.gallery__img}
               src={require(`../../img/gallery/gallery6@2x.jpg`)}
               alt="Ice-cream"
             />
             <img
-              classNames={s.gallery__img}
+              className={s.gallery__img}
               src={require(`../../img/gallery/gallery7@2x.jpg`)}
               alt="Ice-cream"
             />
           </div>
-          <div className={classNames(s.gallery__item, s.gallery__item__pt4)}>
+          <div className={classNames(s.gallery__item, s.gallery__item__pt1)}>
             <img
-              classNames={s.gallery__img}
+              className={s.gallery__img}
               src={require(`../../img/gallery/gallery8@2x.jpg`)}
               alt="Ice-cream"
             />
             <img
-              classNames={s.gallery__img}
+              className={s.gallery__img}
               src={require(`../../img/gallery/gallery9@2x.jpg`)}
               alt="Ice-cream"
             />
           </div>
-          <div className={classNames(s.gallery__item, s.gallery__item__pt4)}>
+          <div className={classNames(s.gallery__item, s.gallery__item__pt5)}>
             <img
-              classNames={s.gallery__img}
+              className={s.gallery__img}
               src={require(`../../img/gallery/gallery10@2x.jpg`)}
               alt="Ice-cream"
             />
